a4aa50a779ac94e8ab3c28afe2eb65f19ee6285d
Volper212 <47470991+Volper212@users.noreply.github.com>
2021-01-27 21:11:16 +0100

    Add convertion function

---
+++ convert.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Kod został opublikowany na licencji JSTMCS (Jeśli Skopiujesz To Mogę Cię Sprzedać).
+Nie biorę odpowiedzialności za jedynki za zadanie.
+Igor Święs
+*/
+
+function toString(int $input, int $base) {
+    if ($base < 2) die("Base must be at least 2");
+    $negative = $input < 0;
+    $input = abs($input);
+    $result = "";
+    do {
+        $result .= $input % $base;
+    } while ($input = intdiv($input, $base));
+    if ($negative) $result .= "-";
+    return strrev($result);
+}
+
+?>
